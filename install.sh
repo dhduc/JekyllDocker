3e5a7374ad78911449a7ea1eaf4b891206c1d93b
Duc Dao <ducdh1@smartosc.com>
2016-12-03 13:05:06 +0700

    Init docker

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WEB='web'
+GIT_REMOTE='git@github.com:dhduc/dhduc.github.io.git'
+GIT_OPTIONS=''
+
+ENDC=`tput setaf 7`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+
+setup() {
+	echo 'Create and clone project to' $WEB 'folder'
+	if [ -d $WEB ]; then
+		echo $RED 'NOTICE: Exist folder ' $WEB $ENDC
+	else
+		git clone $GIT_OPTIONS $GIT_REMOTE $WEB	
+		echo $GREEN 'Clone done to' $WEB 'folder'
+	fi
+}
+
+setup
---
+++ run.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+WEB='web'
+PROJECT='jekyll.local'
+PORT='4000'
+APPNAME='jekyll/jekyll'
+
+ENDC=`tput setaf 7`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+NORMAL=`tput sgr0`
+BOLD=`tput bold`
+
+echo "Start docker services"
+docker-compose up -d
+
+jekyll_conf() {
+	JEKYLL_ID=$(docker ps | grep $APPNAME | awk '{print $1}')
+	if [ -z "$JEKYLL_ID" ]; then
+		echo $RED ERROR: Container \"$PROJECT\" could not be started. $ENDC
+		exit 1
+	fi
+
+	IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $JEKYLL_ID)
+	if [ -z "$IP" ]; then
+		echo $RED ERROR: Could not find the IP address of container \"$PROJECT\". $ENDC
+		exit 1
+	fi
+
+	CONDITION="grep -q '"$PROJECT"' /etc/hosts"
+	if eval $CONDITION; then
+		CMD="sudo sed -i -r \"s/^ *[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+( +"$PROJECT")/"$IP" "$PROJECT"/\" /etc/hosts";
+	else
+		CMD="sudo sed -i '\$a\\\\n# Added automatically by run.sh\n"$IP" "$PROJECT"\n' /etc/hosts";
+	fi
+
+	eval $CMD
+	if [ "$?" -ne 0 ]; then
+		echo $RED ERROR: Could not update $PROJECT to hosts file. $ENDC
+		exit 1
+	fi
+
+	echo $GREEN Project http://$PROJECT:$PORT loaded at $IP $ENDC
+}
+
+nginx_conf() {
+	NGINX_CONTAINER_ID=$(docker ps | grep 'nginx' | awk '{print $1}')
+	if [ -z "$NGINX_CONTAINER_ID" ]; then
+		echo $RED ERROR: Container \"$PROJECT\" could not be started. $ENDC
+		exit 1
+	fi
+
+	IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $NGINX_CONTAINER_ID)
+	if [ -z "$IP" ]; then
+		echo $RED ERROR: Could not find the IP address of container \"$PROJECT\". $ENDC
+		exit 1
+	fi
+
+	echo Attempting to update hosts file [May be need to root password]
+
+	CONDITION="grep -q '"$PROJECT"' /etc/hosts"
+	if eval $CONDITION; then
+		CMD="sudo sed -i -r \"s/^ *[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+( +"$PROJECT")/"$IP" "$PROJECT"/\" /etc/hosts";
+	else
+		CMD="sudo sed -i '\$a\\\\n# Added automatically by run.sh\n"$IP" "$PROJECT"\n' /etc/hosts";
+	fi
+
+	eval $CMD
+	if [ "$?" -ne 0 ]; then
+		echo $RED ERROR: Could not update $PROJECT to hosts file. $ENDC
+		exit 1
+	fi
+
+	echo $GREEN Project http://$PROJECT loaded at $IP $ENDC
+}
+
+# jekyll_conf
+nginx_conf
