6ab12a3bc323ad2fe2585a3aa6a590b32d761fdc
Duc Dao <ducdh1@smartosc.com>
2016-12-18 13:57:59 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -8,18 +8,14 @@ ENDC=`tput setaf 7`
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 
-setup() {
-	if [ -n "$1" ]; then
-    	GIT_REMOTE=$1
-	fi
+if [ -n "$1" ]; then
+	GIT_REMOTE=$1
+fi
 
-	echo 'Create and clone project to' $WEB 'folder'
-	if [ -d $WEB ]; then
-		echo $RED 'NOTICE: Exist folder ' $WEB $ENDC
-	else
-		git clone $GIT_OPTIONS $GIT_REMOTE $WEB	
-		echo $GREEN 'Clone done to' $WEB 'folder' $ENDC
-	fi
-}
-
-setup
+echo 'Create and clone project to' $WEB 'folder'
+if [ -d $WEB ]; then
+	echo $RED 'NOTICE: Exist folder ' $WEB $ENDC
+else
+	git clone $GIT_OPTIONS $GIT_REMOTE $WEB	
+	echo $GREEN 'Clone done to' $WEB 'folder' $ENDC
+fi
